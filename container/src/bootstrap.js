7a9ceb1838918b7f2909b9bcbf0fe13ec89e8b48
flvSantos15 <flvsantos300@gmail.com>
2025-10-17 22:56:47 -0300

    chore: add container setup for sub projects

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'cart/CartShow'
-import 'products/ProductsIndex'
+import { mount as cartMount } from 'cart/CartShow'
+import { mount } from 'products/ProductsIndex'
 
 console.log('container!!')
+
+mount(document.querySelector('#my-products'))
+cartMount(document.querySelector('#my-cart'))
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,22 @@
 import faker from 'faker'
 
-let produtos = ''
+const mount = (el) => {
+  let produtos = ''
 
-for (let index = 0; index < 3; index++) {
-  const name = faker.commerce.productName()
-  produtos += `<div>${name}</div>`
+  for (let index = 0; index < 3; index++) {
+    const name = faker.commerce.productName()
+    produtos += `<div>${name}</div>`
+  }
+
+  el.innerHTML = produtos
 }
 
-document.querySelector('#dev-products').innerHTML = produtos
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products')
+
+  if (el) {
+    mount(el)
+  }
+}
+
+export { mount }
